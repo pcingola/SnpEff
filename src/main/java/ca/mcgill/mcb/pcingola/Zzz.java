aaee5b8a782ea46ddb7f7beb102b682c0bb419cb
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-02-21 10:35:46 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -1,7 +1,6 @@
 package ca.mcgill.mcb.pcingola;
 
 import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
-import ca.mcgill.mcb.pcingola.interval.SeqChange;
 import ca.mcgill.mcb.pcingola.util.Gpr;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
@@ -9,14 +8,10 @@ public class Zzz {
 
 	public static void main(String[] args) {
 
-		VcfFileIterator vcf = new VcfFileIterator(Gpr.HOME + "/snpEff/aa.vcf");
+		VcfFileIterator vcf = new VcfFileIterator(Gpr.HOME + "/snpEff/test_tabix.vcf.gz");
 
 		for (VcfEntry ve : vcf) {
 			System.out.println(ve);
-
-			for (SeqChange sc : ve.seqChanges()) {
-				System.out.println("\t" + sc);
-			}
 		}
 	}
 
