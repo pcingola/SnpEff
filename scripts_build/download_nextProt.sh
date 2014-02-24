e5f5a3a304aa811458e50b10ca5e46a3b21f8317
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-02-23 20:06:05 -0500

    Project updated

--- scripts_build/download_nextProt.sh
+++ scripts_build/download_nextProt.sh
@@ -3,14 +3,10 @@
 mkdir -p db/nextProt/
 cd db/nextProt/
 
-#for chr in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 21 22 X Y MT
-#do
-#	wget "ftp://ftp.nextprot.org/pub/current_release/xml/nextprot_chromosome_$chr.xml.gz"
-#done
-#
-#echo "Uncompressing files"
-#gunzip -v *gz
+for chr in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 21 22 X Y MT
+do
+	wget "ftp://ftp.nextprot.org/pub/current_release/xml/nextprot_chromosome_$chr.xml.gz"
+done
 
-wget "ftp://ftp.nextprot.org/pub/current_release/xml/nextprot_all.xml.gz"
 echo "Uncompressing files"
-gunzip -v nextprot_all.xml.gz
+gunzip -v *gz
