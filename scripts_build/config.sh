f82508d4849c0ec134ae315a72306e8e38b806b1
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-03-10 10:27:37 -0400

    Project updated

--- scripts_build/config.sh
+++ scripts_build/config.sh
@@ -5,7 +5,7 @@ export SUBVERSION_SNPEFF=""
 export VERSION_SNPSIFT=$VERSION_SNPEFF
 export VERSION_SNPSQL=0.2
 
-export ENSEMBL_RELEASE=74
+export ENSEMBL_RELEASE=75
 export ENSEMBL_BFMPP_RELEASE=21
 
 # Version values using underscores ('3_2' instead of '3.2')
