e109850eb4b5ffa843a1884da0201533918ca329
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-03-11 08:06:50 -0400

    Project updated

--- scripts_build/build_dbNSFP.sh
+++ scripts_build/build_dbNSFP.sh
@@ -4,32 +4,32 @@ base="dbNSFP2.4"
 db="$base.txt"
 
 echo Create file $db
-head -n 1 $base_variant.chr1 > $db
+head -n 1 $base\_variant.chr1 > $db
 
-cat $base_variant.chr1 \
-		$base_variant.chr2 \
-		$base_variant.chr3 \
-		$base_variant.chr4 \
-		$base_variant.chr5 \
-		$base_variant.chr6 \
-		$base_variant.chr7 \
-		$base_variant.chr8 \
-		$base_variant.chr9 \
-		$base_variant.chr10 \
-		$base_variant.chr11 \
-		$base_variant.chr12 \
-		$base_variant.chr13 \
-		$base_variant.chr14 \
-		$base_variant.chr15 \
-		$base_variant.chr16 \
-		$base_variant.chr17 \
-		$base_variant.chr18 \
-		$base_variant.chr19 \
-		$base_variant.chr20 \
-		$base_variant.chr21 \
-		$base_variant.chr22 \
-		$base_variant.chrX \
-		$base_variant.chrY \
+cat $base\_variant.chr1 \
+		$base\_variant.chr2 \
+		$base\_variant.chr3 \
+		$base\_variant.chr4 \
+		$base\_variant.chr5 \
+		$base\_variant.chr6 \
+		$base\_variant.chr7 \
+		$base\_variant.chr8 \
+		$base\_variant.chr9 \
+		$base\_variant.chr10 \
+		$base\_variant.chr11 \
+		$base\_variant.chr12 \
+		$base\_variant.chr13 \
+		$base\_variant.chr14 \
+		$base\_variant.chr15 \
+		$base\_variant.chr16 \
+		$base\_variant.chr17 \
+		$base\_variant.chr18 \
+		$base\_variant.chr19 \
+		$base\_variant.chr20 \
+		$base\_variant.chr21 \
+		$base\_variant.chr22 \
+		$base\_variant.chrX \
+		$base\_variant.chrY \
 	| grep -v "^#" \
 	>> $db
 
