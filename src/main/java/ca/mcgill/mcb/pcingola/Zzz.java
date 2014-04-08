ca87488de8d28e975ae1b5a20c6fd316389f7ec6
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-04-08 16:54:47 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -1,46 +1,59 @@
 package ca.mcgill.mcb.pcingola;
 
-import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
-import ca.mcgill.mcb.pcingola.interval.Marker;
-import ca.mcgill.mcb.pcingola.interval.NextProt;
+import ca.mcgill.mcb.pcingola.interval.Gene;
+import ca.mcgill.mcb.pcingola.interval.Intron;
+import ca.mcgill.mcb.pcingola.interval.Transcript;
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
 import ca.mcgill.mcb.pcingola.util.Gpr;
 import ca.mcgill.mcb.pcingola.util.Timer;
-import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 public class Zzz extends SnpEff {
+
+	StringBuilder sb = new StringBuilder();
 
 	public static void main(String[] args) {
 		Timer.showStdErr("Start");
 
-		String vcfFile = Gpr.HOME + "/snpEff/slow.vcf";
-
-		VcfFileIterator vcf = new VcfFileIterator(vcfFile);
-		for (VcfEntry ve : vcf) {
-			System.out.println(ve);
-		}
-
+		Zzz zzz = new Zzz(null);
+		zzz.load("testHg3771Chr1");
+		zzz.run();
 		Timer.showStdErr("End");
 	}
 
 	public Zzz(String[] args) {
 		super(args);
+		verbose = true;
 	}
 
 	void load(String genome) {
 		setGenomeVer(genome);
-		parseArgs(args);
+		//		parseArgs(args);
 		loadConfig();
+		canonical = true;
 		loadDb();
 	}
 
 	@Override
 	public boolean run() {
-		for (Marker m : config.getSnpEffectPredictor().getMarkers()) {
-			if (m instanceof NextProt) {
-				System.out.println(m);
+		for (Gene g : config.getSnpEffectPredictor().getGenome().getGenes()) {
+			//			System.out.println(g.getGeneName());
+			for (Transcript tr : g) {
+				if (!tr.isProteinCoding()) continue;
+				if (tr.introns().size() < 2) continue;
+
+				// System.out.println("\t" + tr.getId());
+				for (Intron i : tr.introns()) {
+					int pos = i.getStart() + (int) (Math.random() * (i.size() - 2)) + 1;
+
+					String line = i.getChromosomeName() + "\t" + pos + "\t.\tA\tT\t.\t.\tAC=1;GENE=" + g.getGeneName() + ";TR=" + tr.getId() + ";INTRON=" + i.getRank();
+					System.out.println(line);
+					sb.append(line + "\n");
+				}
 			}
 		}
+
+		Gpr.toFile(Gpr.HOME + "/introns_test.vcf", sb);
+
 		return true;
 	}
 }
