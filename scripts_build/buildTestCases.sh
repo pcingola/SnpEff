e1dfcf1c0b30c98d23d1ca3b2a710bb2a1a67d30
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-06-25 12:45:42 -0400

    Project updated

--- scripts_build/buildTestCases.sh
+++ scripts_build/buildTestCases.sh
@@ -2,39 +2,31 @@
 
 SNPEFF="java -Xmx2G -jar snpEff.jar"
 
+#---
+# Build "special" cases
+#---
 # Test cases hg37
 $SNPEFF build -noLog -txt testCase
 
-# Test cases hg37.61
-$SNPEFF build -noLog -gtf22 testHg3761Chr15
-$SNPEFF build -noLog -gtf22 testHg3761Chr16 
-
-# Test cases hg37.63
-$SNPEFF build -noLog -gtf22 testHg3763Chr1
-$SNPEFF build -noLog -gtf22 testHg3763Chr20 
-$SNPEFF build -noLog -gtf22 testHg3763ChrY 
-
-# Test cases hg37.65
-$SNPEFF build -noLog -gtf22 testHg3765Chr22
-
-# Test cases hg37.66
-$SNPEFF build -noLog -gtf22 testHg3766Chr1
-
-# Test cases hg37.67
-$SNPEFF build -noLog -gtf22 testHg3767Chr21Mt
-
-# Test cases hg37.69
-$SNPEFF build -noLog -gtf22 testHg3769Chr12
+# Test cases 
+$SNPEFF build -noLog -gff3 testLukas
 
 # Test cases hg37.70
 $SNPEFF build -noLog -gtf22 testHg3770Chr22
 cp db/jaspar/pwms.bin data/testHg3770Chr22/
 
-# Test cases hg37.71
-$SNPEFF build -noLog -gtf22 testHg3771Chr1
+# Test case testHg19Chr1
+snpeff build -noLog -refSeq testHg19Chr1
 
-# Test cases 
-$SNPEFF build -noLog -gff3 testLukas
+#---
+# Buils all GTF 2.2
+#---
+for gen in testHg3761Chr15 testHg3761Chr16 testHg3763Chr1 testHg3763Chr20 testHg3763ChrY testHg3765Chr22 testHg3766Chr1 testHg3767Chr21Mt testHg3769Chr12 testHg3771Chr1 testHg3775Chr1 testENST00000268124
+do
+	echo
+	echo
+	echo Genome: $gen
+	$SNPEFF build -noLog -gtf22 testHg3761Chr15
+done
 
-# Test case testHg3775Chr1
-$SNPEFF build -noLog -gtf22 testHg3775Chr1
+
