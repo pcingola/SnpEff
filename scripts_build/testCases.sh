5a4e72cdd8e2469c5b909348dc431155b37479f6
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-08 14:26:47 -0400

    Project updated

--- scripts_build/testCases.sh
+++ scripts_build/testCases.sh
@@ -3,11 +3,15 @@
 # Build JAR files
 # ./scripts_build/make.sh
 
+DIR=$HOME/snpEff
+
 # Run SnpSift test cases
 cd $HOME/workspace/SnpSift/
-java -Xmx4g -cp $HOME/snpEff/SnpSift.jar org.junit.runner.JUnitCore ca.mcgill.mcb.pcingola.snpSift.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpsift.txt
+java -Xmx4g -cp $DIR/SnpSift.jar org.junit.runner.JUnitCore ca.mcgill.mcb.pcingola.snpSift.testCases.TestSuiteAll 2>&1 | tee $DIR/testSuiteAll.snpsift.txt
+#java -Xmx4g -cp ~/snpEff/SnpSift.jar ca.mcgill.mcb.pcingola.snpSift.testCases.TestSuiteAll 2>&1 | tee TestSuiteAll.out
 
-# Run SnpEff test cases
-cd $HOME/workspace/SnpEff/
-java -Xmx4g -cp $HOME/snpEff/snpEff.jar ca.mcgill.mcb.pcingola.snpEffect.testCases.TestSuiteAll 2>&1 | tee testSuiteAll.snpeff.txt
+
+## Run SnpEff test cases
+#cd $HOME/workspace/SnpEff/
+#java -Xmx4g -cp $DIR/snpEff.jar ca.mcgill.mcb.pcingola.snpEffect.testCases.TestSuiteAll 2>&1 | tee $DIR/testSuiteAll.snpeff.txt
 
