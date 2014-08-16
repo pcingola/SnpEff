36294615d245f8b205e92f6eb73d210d49682c84
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-16 11:12:32 -0400

    Project updated

--- scripts_build/config.sh
+++ scripts_build/config.sh
@@ -12,3 +12,7 @@ export SNPEFF_VERSION=`echo $VERSION_SNPEFF | tr "." "_"`
 export SNPSIFT_VERSION=`echo $VERSION_SNPSIFT | tr "." "_"`
 
 export SNPEFF_VERSION_REV=$SNPEFF_VERSION"$SUBVERSION_SNPEFF"
+
+# HUman and mouse versions
+GRCH=GRCh38
+GRCM=GRCm38
