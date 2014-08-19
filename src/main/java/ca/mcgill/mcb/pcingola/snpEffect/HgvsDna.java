a6b31f4c1b7517b1381bd748755811987ac4c4a5
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-19 11:45:24 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/HgvsDna.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/HgvsDna.java
@@ -109,33 +109,33 @@ public class HgvsDna extends Hgvs {
 			}
 		}
 
+		if (tr == null) return null;
 		String posPrepend = "";
 
 		// Exon position
-		int codonNum = variantEffect.getCodonNum();
+		int codonNum = -1; // variantEffect.getCodonNum();
 		int posStart = -1, posEnd = -1;
-		if (codonNum >= 0) posStart = codonNum * 3 + variantEffect.getCodonIndex() + 1;
-		else {
-			if (tr == null) return null;
+		// if (codonNum >= 0) posStart = codonNum * 3 + variantEffect.getCodonIndex() + 1;
+		// else {
 
-			int variantPos = tr.isStrandPlus() ? variant.getStart() : variant.getEnd();
-			if (variantEffect.isUtr3()) {
-				// We are after stop codon, coordinates must be '*1', '*2', etc.
-				int pos = tr.baseNumberPreMRna(variantPos);
-				int posStop = tr.baseNumberPreMRna(tr.getCdsEnd());
-				posStart = Math.abs(pos - posStop);
-				posPrepend = "*";
-			} else if (variantEffect.isUtr5()) {
-				// We are before TSS, coordinates must be '-1', '-2', etc.
-				int pos = tr.baseNumberPreMRna(variantPos);
-				int posTss = tr.baseNumberPreMRna(tr.getCdsStart());
-				posStart = Math.abs(pos - posTss);
-				posPrepend = "-";
-			} else {
-				posStart = tr.baseNumberCds(variantPos, false) + 1;
-				// posStart = tr.baseNumberPreMRna(variant.getStart()) + 1;
-			}
+		int variantPos = tr.isStrandPlus() ? variant.getStart() : variant.getEnd();
+		if (variantEffect.isUtr3()) {
+			// We are after stop codon, coordinates must be '*1', '*2', etc.
+			int pos = tr.baseNumberPreMRna(variantPos);
+			int posStop = tr.baseNumberPreMRna(tr.getCdsEnd());
+			posStart = Math.abs(pos - posStop);
+			posPrepend = "*";
+		} else if (variantEffect.isUtr5()) {
+			// We are before TSS, coordinates must be '-1', '-2', etc.
+			int pos = tr.baseNumberPreMRna(variantPos);
+			int posTss = tr.baseNumberPreMRna(tr.getCdsStart());
+			posStart = Math.abs(pos - posTss);
+			posPrepend = "-";
+		} else {
+			posStart = tr.baseNumberCds(variantPos, false) + 1;
+			// posStart = tr.baseNumberPreMRna(variant.getStart()) + 1;
 		}
+		// }
 
 		// Could not find dna position in transcript?
 		if (posStart <= 0) return null;
