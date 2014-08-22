34d91e946a9fe2c4fd8dd34889cd270ff322fcd4
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-22 11:48:06 -0400

    Project updated

--- scripts_build/make_config.sh
+++ scripts_build/make_config.sh
@@ -7,12 +7,6 @@
 cat config/snpEff.core.config \
 	config/snpEff.dbs.config \
 	config/snpEff.test.config \
-	config/snpEff.ENSEMBL_64.config \
-	config/snpEff.ENSEMBL_65.config \
-	config/snpEff.ENSEMBL_66.config \
-	config/snpEff.ENSEMBL_67.config \
-	config/snpEff.ENSEMBL_68.config \
-	config/snpEff.ENSEMBL_69.config \
 	config/snpEff.ENSEMBL_70.config \
 	config/snpEff.ENSEMBL_71.config \
 	config/snpEff.ENSEMBL_72.config \
