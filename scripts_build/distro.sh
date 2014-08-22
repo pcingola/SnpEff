0d7a5e093f9ca7c69880d915047f046b6f5db69f
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-08-22 17:04:44 -0400

    Project updated

--- scripts_build/distro.sh
+++ scripts_build/distro.sh
@@ -26,7 +26,7 @@ zip -r $ZIP snpEff
 
 # Create ZIP file for each database
 # for d in `ls data/Brachyspira_hyodysenteriae_WA1_uid59291/snpEffectPredictor.bin data/Candidatus_Hamiltonella_defensa_5AT__Acyrthosiphon_pisum__uid59289/snpEffectPredictor.bin data/Erwinia_amylovora_ATCC_49946_uid46943/snpEffectPredictor.bin  data/Gordonia_KTR9_uid174812/snpEffectPredictor.bin  data/Haemophilus_influenzae_R2866_uid161923/snpEffectPredictor.bin  data/Lactobacillus_salivarius_UCC118_uid58233/snpEffectPredictor.bin  data/NC_009089.1/snpEffectPredictor.bin data/NC_014033.1/snpEffectPredictor.bin data/Petromyzon_marinus_7.0.65/snpEffectPredictor.bin  data/Saccharopolyspora_erythraea_NRRL_2338_uid62947/snpEffectPredictor.bin data/Salmonella_enterica_serovar_Typhimurium_LT2_uid57799/snpEffectPredictor.bin data/Shewanella_piezotolerans_WP3_uid58745/snpEffectPredictor.bin data/Staphylococcus_aureus_04_02981_uid161969/snpEffectPredictor.bin data/Staphylococcus_aureus_CP000730_USA300_TCH1516/snpEffectPredictor.bin data/Streptococcus_pyogenes_M1_GAS_uid57845/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS10270_uid58571/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS10750_uid58575/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS2096_uid58573/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS5005_uid58337/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS6180_uid58335/snpEffectPredictor.bin data/Streptococcus_pyogenes_MGAS9429_uid58569/snpEffectPredictor.bin ` 
-for d in `ls data/GCA_000093125.1.18*/snpEffectPredictor.bin`
+for d in `ls data/*/snpEffectPredictor.bin`
 do
 	DIR=`dirname $d`
 	GEN=`basename $DIR`
