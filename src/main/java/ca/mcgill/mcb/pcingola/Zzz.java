edd027cfed6ed481b1e8f611185f47e9d4ab9e35
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-09-12 20:54:49 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -1,21 +1,12 @@
 package ca.mcgill.mcb.pcingola;
 
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
-import ca.mcgill.mcb.pcingola.util.Download;
 import ca.mcgill.mcb.pcingola.util.Timer;
 
 public class Zzz extends SnpEff {
 
 	public static void main(String[] args) {
 		Timer.showStdErr("Start");
-
-		Download download = new Download();
-		download.setVerbose(true);
-
-		String zipFile = "/home/pcingola/zzz/file.zip";
-		String mainDir = "/home/pcingola/zzz/main";
-		String dataDir = "/home/pcingola/zzz/main/data";
-		download.unzip(zipFile, mainDir, dataDir);
 
 		Timer.showStdErr("End");
 	}
