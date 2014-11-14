6f6e1d29bd0535d11a630aae9144c3a06b0246f6
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-14 18:30:10 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/TestCasesZzz.java
@@ -59,4 +59,39 @@ public class TestCasesZzz extends TestCase {
 
 		Assert.assertTrue(ok);
 	}
+
+	//	/**
+	//	 * Compare to SAVANT's case (poster on BRCA2 mutations)
+	//	 */
+	//	public void test_16_savant() {
+	//		Gpr.debug("Test");
+	//		String genome = "testHg19Chr13";
+	//		String vcf = "tests/hgvs_savant.vcf";
+	//
+	//		// Create SnpEff
+	//		String args[] = { genome, vcf };
+	//		SnpEffCmdEff snpeff = new SnpEffCmdEff();
+	//		snpeff.parseArgs(args);
+	//		snpeff.setDebug(debug);
+	//		snpeff.setVerbose(verbose);
+	//		snpeff.setSupressOutput(!verbose);
+	//
+	//		// The problem appears when splice site is large (in this example)
+	//		snpeff.setUpDownStreamLength(0);
+	//
+	//		// Run & get result (single line)
+	//		List<VcfEntry> results = snpeff.run(true);
+	//		VcfEntry ve = results.get(0);
+	//
+	//		// Make sure the spleice site is annotatted as "c.1909+12delT" (instead of "c.1910delT")
+	//		boolean ok = false;
+	//		for (VcfEffect veff : ve.parseEffects()) {
+	//			if (verbose) System.out.println("\t" + veff + "\t" + veff.getEffectsStr() + "\t" + veff.getHgvsDna());
+	//			ok |= veff.getEffectsStr().equals("SPLICE_SITE_REGION") //
+	//					&& veff.getTranscriptId().equals("NM_001232.3") //
+	//					&& veff.getHgvsDna().equals("c.420+6T>C");
+	//		}
+	//
+	//		Assert.assertTrue(ok);
+	//	}
 }
