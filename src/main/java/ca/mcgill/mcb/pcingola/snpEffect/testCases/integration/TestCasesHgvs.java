ff7b6d1345f33fbeef7023d8ff243cb7d63c72f2
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-29 09:31:38 -0500

    Test cases TestCasesHgvs taking too much time: Fixed

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesHgvs.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesHgvs.java
@@ -8,12 +8,18 @@ import junit.framework.TestCase;
 
 import org.junit.Assert;
 
+import ca.mcgill.mcb.pcingola.codons.CodonTable;
 import ca.mcgill.mcb.pcingola.interval.Chromosome;
+import ca.mcgill.mcb.pcingola.interval.Exon;
 import ca.mcgill.mcb.pcingola.interval.Gene;
 import ca.mcgill.mcb.pcingola.interval.Genome;
 import ca.mcgill.mcb.pcingola.interval.Transcript;
+import ca.mcgill.mcb.pcingola.interval.Variant;
 import ca.mcgill.mcb.pcingola.snpEffect.Config;
+import ca.mcgill.mcb.pcingola.snpEffect.EffectType;
 import ca.mcgill.mcb.pcingola.snpEffect.SnpEffectPredictor;
+import ca.mcgill.mcb.pcingola.snpEffect.VariantEffect;
+import ca.mcgill.mcb.pcingola.snpEffect.VariantEffects;
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEffCmdEff;
 import ca.mcgill.mcb.pcingola.snpEffect.factory.SnpEffPredictorFactoryRand;
@@ -78,7 +84,7 @@ public class TestCasesHgvs extends TestCase {
 	 */
 	void initSnpEffPredictor(boolean addUtrs, boolean onlyPlusStrand) {
 		// Create a config and force out snpPredictor for hg37 chromosome Y
-		config = new Config("testCase", Config.DEFAULT_CONFIG_FILE);
+		if (config == null) config = new Config("testCase", Config.DEFAULT_CONFIG_FILE);
 
 		// Initialize factory
 		int maxGeneLen = 1000;
@@ -215,189 +221,189 @@ public class TestCasesHgvs extends TestCase {
 		}
 	}
 
-	//	public void test_01_coding() {
-	//		Gpr.debug("Test");
-	//		int N = 250;
-	//		CodonTable codonTable = genome.codonTable();
-	//
-	//		if (skipLong) throw new RuntimeException("Test skipped!");
-	//
-	//		// Test N times
-	//		//	- Create a random gene transcript, exons
-	//		//	- Change each base in the exon
-	//		//	- Calculate effect
-	//		for (int i = 0; i < N; i++) {
-	//			initSnpEffPredictor(false, true);
-	//			if (debug) System.out.println("HGSV Test iteration: " + i + "\n" + transcript);
-	//			else if (verbose) System.out.println("HGSV Coding\titeration: " + i + "\t" + (transcript.isStrandPlus() ? "+" : "-") + "\t" + transcript.cds());
-	//			else Gpr.showMark(i + 1, 1);
-	//
-	//			int cdsBaseNum = 0;
-	//
-	//			// For each exon...
-	//			for (Exon exon : transcript.sortedStrand()) {
-	//				// For each base in this exon...
-	//				for (int pos = exon.getStart(); (exon.getStart() <= pos) && (pos <= exon.getEnd()); pos++, cdsBaseNum++) {
-	//
-	//					// Reference base
-	//					char refBase = chromoBases[pos]; // exon.basesAt(pos - exon.getStart(), 1).charAt(0);
-	//					refBase = Character.toUpperCase(refBase);
-	//					// Codon number
-	//					int cdsCodonNum = cdsBaseNum / 3;
-	//					int cdsCodonPos = cdsBaseNum % 3;
-	//
-	//					int minCodonPos = cdsCodonNum * 3;
-	//					int maxCodonPos = minCodonPos + 3;
-	//					if (maxCodonPos < transcript.cds().length()) {
-	//						String codon = transcript.cds().substring(minCodonPos, maxCodonPos);
-	//						codon = codon.toUpperCase();
-	//						String aa = codonTable.aaThreeLetterCode(codonTable.aa(codon));
-	//
-	//						// Get a random base different from 'refBase'
-	//						char snp = refBase;
-	//						while (snp == refBase)
-	//							snp = Character.toUpperCase(GprSeq.randBase(rand));
-	//
-	//						// Codon change
-	//						String newCodon = codon.substring(0, cdsCodonPos) + snp + codon.substring(cdsCodonPos + 1);
-	//						String newAa = codonTable.aaThreeLetterCode(codonTable.aa(newCodon));
-	//						String protHgvs = "";
-	//						String dnaHgvs = "c." + (cdsBaseNum + 1) + refBase + ">" + snp;
-	//
-	//						// Effect
-	//						if (newAa.equals(aa)) {
-	//							if ((cdsCodonNum == 0) && (codonTable.isStart(codon))) {
-	//								if (codonTable.isStart(newCodon)) protHgvs = "p." + aa + "1?";
-	//								else protHgvs = "p." + aa + "1?";
-	//							} else protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa;
-	//						} else {
-	//							if ((cdsCodonNum == 0) && (codonTable.isStart(codon))) {
-	//								if (codonTable.isStart(newCodon)) protHgvs = "p." + aa + "1?";
-	//								else protHgvs = "p." + aa + "1?";
-	//							} else if (codonTable.isStop(codon)) protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa + "ext*?";
-	//							else if (codonTable.isStop(newCodon)) protHgvs = "p." + aa + (cdsCodonNum + 1) + "*";
-	//							else protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa;
-	//						}
-	//
-	//						String effectExpected = protHgvs + "/" + dnaHgvs;
-	//
-	//						// Create a SeqChange
-	//						Variant seqChange = new Variant(chromosome, pos, refBase + "", snp + "", "");
-	//
-	//						if (!seqChange.isVariant()) protHgvs = "EXON";
-	//
-	//						// Calculate effects
-	//						VariantEffects effects = snpEffectPredictor.variantEffect(seqChange);
-	//
-	//						// There should be only one effect
-	//						Assert.assertEquals(true, effects.size() <= 1);
-	//
-	//						// Show
-	//						if (effects.size() == 1) {
-	//							VariantEffect effect = effects.get();
-	//							String effStr = effect.getHgvs();
-	//
-	//							if (debug) System.out.println("\tPos: " + pos //
-	//									+ "\tCDS base num: " + cdsBaseNum + " [" + cdsCodonNum + ":" + cdsCodonPos + "]" //
-	//									+ "\t" + seqChange + (seqChange.isStrandPlus() ? "+" : "-") //
-	//									+ "\tCodon: " + codon + " -> " + newCodon //
-	//									+ "\tAA: " + aa + " -> " + newAa //
-	//									+ "\tEffect expected: " + effectExpected //
-	//									+ "\tEffect: " + effStr);
-	//
-	//							// Check effect
-	//							Assert.assertEquals(effectExpected, effStr);
-	//
-	//							// Check that effect can be inserted into a VCF field
-	//							if (!VcfEntry.isValidInfoValue(effStr)) {
-	//								String err = "No white-space, semi-colons, or equals-signs are permitted in INFO field. Value:\"" + effStr + "\"";
-	//								System.err.println(err);
-	//								throw new RuntimeException(err);
-	//							}
-	//
-	//						}
-	//					}
-	//				}
-	//			}
-	//		}
-	//		System.err.println("");
-	//	}
-	//
-	//	public void test_01_intron() {
-	//		Gpr.debug("Test");
-	//		int N = 250;
-	//
-	//		int testIter = -1;
-	//		int testPos = -1;
-	//
-	//		if (skipLong) throw new RuntimeException("Test skipped!");
-	//
-	//		// Test N times
-	//		//	- Create a random gene transcript, exons
-	//		//	- Change each base in the exon
-	//		//	- Calculate effect
-	//		for (int checked = 0, it = 1; checked < N; it++) {
-	//			initSnpEffPredictor(true, false);
-	//			boolean tested = false;
-	//
-	//			// Skip test?
-	//			if (testIter >= 0 && it < testIter) {
-	//				Gpr.debug("Skipping iteration: " + it);
-	//				continue;
-	//			}
-	//
-	//			// No introns? Nothing to test
-	//			if (transcript.introns().size() < 1) continue;
-	//
-	//			// Character representation
-	//			String trstr = transcript.toStringAsciiArt();
-	//			char bases[] = trstr.toCharArray();
-	//
-	//			// Show data
-	//			if (verbose) {
-	//				System.out.println("HGSV Intron\titeration:" + checked + "\t" + (transcript.isStrandPlus() ? "+" : "-"));
-	//				System.out.println(trstr);
-	//				System.out.println("Length   : " + transcript.size());
-	//				System.out.println("CDS start: " + transcript.getCdsStart());
-	//				System.out.println("CDS end  : " + transcript.getCdsEnd());
-	//				System.out.println(transcript);
-	//			} else Gpr.showMark(it, 1);
-	//
-	//			// Check each intronic base
-	//			for (int j = 0, pos = transcript.getStart(); pos < transcript.getEnd(); j++, pos++) {
-	//				// Intron?
-	//				if (bases[j] == '-') {
-	//					tested = true;
-	//
-	//					// Skip base?
-	//					if (testPos >= 0 && pos < testPos) {
-	//						Gpr.debug("\tSkipping\tpos: " + pos + " [" + j + "]");
-	//						continue;
-	//					}
-	//
-	//					// Ref & Alt
-	//					String refStr = "A", altStr = "T";
-	//
-	//					// Calculate expected hgsv string
-	//					String hgsv = intronHgsv(bases, j, pos, refStr, altStr);
-	//
-	//					// Calculate effect and compare to expected
-	//					Variant sc = new Variant(transcript.getChromosome(), pos, refStr, altStr, "");
-	//					VariantEffects ceffs = snpEffectPredictor.variantEffect(sc);
-	//					VariantEffect ceff = ceffs.get();
-	//					String hgsvEff = ceffs.get().getHgvs();
-	//					if (debug) System.out.println("\tpos: " + pos + " [" + j + "]\thgsv: '" + hgsv + "'\tEff: '" + hgsvEff + "'\t" + ceff.getEffectType());
-	//
-	//					// Is this an intron? (i.e. skip other effects, such as splice site)
-	//					// Compare expected to real HGSV strings
-	//					if (ceff.getEffectType() == EffectType.INTRON) Assert.assertEquals(hgsv, hgsvEff);
-	//				}
-	//			}
-	//
-	//			if (tested) checked++;
-	//		}`
-	//		System.err.println("");
-	//	}
+	public void test_01_coding() {
+		Gpr.debug("Test");
+		int N = 250;
+		CodonTable codonTable = genome.codonTable();
+
+		if (skipLong) throw new RuntimeException("Test skipped!");
+
+		// Test N times
+		//	- Create a random gene transcript, exons
+		//	- Change each base in the exon
+		//	- Calculate effect
+		for (int i = 0; i < N; i++) {
+			initSnpEffPredictor(false, true);
+			if (debug) System.out.println("HGSV Test iteration: " + i + "\n" + transcript);
+			else if (verbose) System.out.println("HGSV Coding\titeration: " + i + "\t" + (transcript.isStrandPlus() ? "+" : "-") + "\t" + transcript.cds());
+			else Gpr.showMark(i + 1, 1);
+
+			int cdsBaseNum = 0;
+
+			// For each exon...
+			for (Exon exon : transcript.sortedStrand()) {
+				// For each base in this exon...
+				for (int pos = exon.getStart(); (exon.getStart() <= pos) && (pos <= exon.getEnd()); pos++, cdsBaseNum++) {
+
+					// Reference base
+					char refBase = chromoBases[pos]; // exon.basesAt(pos - exon.getStart(), 1).charAt(0);
+					refBase = Character.toUpperCase(refBase);
+					// Codon number
+					int cdsCodonNum = cdsBaseNum / 3;
+					int cdsCodonPos = cdsBaseNum % 3;
+
+					int minCodonPos = cdsCodonNum * 3;
+					int maxCodonPos = minCodonPos + 3;
+					if (maxCodonPos < transcript.cds().length()) {
+						String codon = transcript.cds().substring(minCodonPos, maxCodonPos);
+						codon = codon.toUpperCase();
+						String aa = codonTable.aaThreeLetterCode(codonTable.aa(codon));
+
+						// Get a random base different from 'refBase'
+						char snp = refBase;
+						while (snp == refBase)
+							snp = Character.toUpperCase(GprSeq.randBase(rand));
+
+						// Codon change
+						String newCodon = codon.substring(0, cdsCodonPos) + snp + codon.substring(cdsCodonPos + 1);
+						String newAa = codonTable.aaThreeLetterCode(codonTable.aa(newCodon));
+						String protHgvs = "";
+						String dnaHgvs = "c." + (cdsBaseNum + 1) + refBase + ">" + snp;
+
+						// Effect
+						if (newAa.equals(aa)) {
+							if ((cdsCodonNum == 0) && (codonTable.isStart(codon))) {
+								if (codonTable.isStart(newCodon)) protHgvs = "p." + aa + "1?";
+								else protHgvs = "p." + aa + "1?";
+							} else protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa;
+						} else {
+							if ((cdsCodonNum == 0) && (codonTable.isStart(codon))) {
+								if (codonTable.isStart(newCodon)) protHgvs = "p." + aa + "1?";
+								else protHgvs = "p." + aa + "1?";
+							} else if (codonTable.isStop(codon)) protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa + "ext*?";
+							else if (codonTable.isStop(newCodon)) protHgvs = "p." + aa + (cdsCodonNum + 1) + "*";
+							else protHgvs = "p." + aa + (cdsCodonNum + 1) + newAa;
+						}
+
+						String effectExpected = protHgvs + "/" + dnaHgvs;
+
+						// Create a SeqChange
+						Variant seqChange = new Variant(chromosome, pos, refBase + "", snp + "", "");
+
+						if (!seqChange.isVariant()) protHgvs = "EXON";
+
+						// Calculate effects
+						VariantEffects effects = snpEffectPredictor.variantEffect(seqChange);
+
+						// There should be only one effect
+						Assert.assertEquals(true, effects.size() <= 1);
+
+						// Show
+						if (effects.size() == 1) {
+							VariantEffect effect = effects.get();
+							String effStr = effect.getHgvs();
+
+							if (debug) System.out.println("\tPos: " + pos //
+									+ "\tCDS base num: " + cdsBaseNum + " [" + cdsCodonNum + ":" + cdsCodonPos + "]" //
+									+ "\t" + seqChange + (seqChange.isStrandPlus() ? "+" : "-") //
+									+ "\tCodon: " + codon + " -> " + newCodon //
+									+ "\tAA: " + aa + " -> " + newAa //
+									+ "\tEffect expected: " + effectExpected //
+									+ "\tEffect: " + effStr);
+
+							// Check effect
+							Assert.assertEquals(effectExpected, effStr);
+
+							// Check that effect can be inserted into a VCF field
+							if (!VcfEntry.isValidInfoValue(effStr)) {
+								String err = "No white-space, semi-colons, or equals-signs are permitted in INFO field. Value:\"" + effStr + "\"";
+								System.err.println(err);
+								throw new RuntimeException(err);
+							}
+
+						}
+					}
+				}
+			}
+		}
+		System.err.println("");
+	}
+
+	public void test_01_intron() {
+		Gpr.debug("Test");
+		int N = 250;
+
+		int testIter = -1;
+		int testPos = -1;
+
+		if (skipLong) throw new RuntimeException("Test skipped!");
+
+		// Test N times
+		//	- Create a random gene transcript, exons
+		//	- Change each base in the exon
+		//	- Calculate effect
+		for (int checked = 0, it = 1; checked < N; it++) {
+			initSnpEffPredictor(true, false);
+			boolean tested = false;
+
+			// Skip test?
+			if (testIter >= 0 && it < testIter) {
+				Gpr.debug("Skipping iteration: " + it);
+				continue;
+			}
+
+			// No introns? Nothing to test
+			if (transcript.introns().size() < 1) continue;
+
+			// Character representation
+			String trstr = transcript.toStringAsciiArt();
+			char bases[] = trstr.toCharArray();
+
+			// Show data
+			if (verbose) {
+				System.out.println("HGSV Intron\titeration:" + checked + "\t" + (transcript.isStrandPlus() ? "+" : "-"));
+				System.out.println(trstr);
+				System.out.println("Length   : " + transcript.size());
+				System.out.println("CDS start: " + transcript.getCdsStart());
+				System.out.println("CDS end  : " + transcript.getCdsEnd());
+				System.out.println(transcript);
+			} else Gpr.showMark(it, 1);
+
+			// Check each intronic base
+			for (int j = 0, pos = transcript.getStart(); pos < transcript.getEnd(); j++, pos++) {
+				// Intron?
+				if (bases[j] == '-') {
+					tested = true;
+
+					// Skip base?
+					if (testPos >= 0 && pos < testPos) {
+						Gpr.debug("\tSkipping\tpos: " + pos + " [" + j + "]");
+						continue;
+					}
+
+					// Ref & Alt
+					String refStr = "A", altStr = "T";
+
+					// Calculate expected hgsv string
+					String hgsv = intronHgsv(bases, j, pos, refStr, altStr);
+
+					// Calculate effect and compare to expected
+					Variant sc = new Variant(transcript.getChromosome(), pos, refStr, altStr, "");
+					VariantEffects ceffs = snpEffectPredictor.variantEffect(sc);
+					VariantEffect ceff = ceffs.get();
+					String hgsvEff = ceffs.get().getHgvs();
+					if (debug) System.out.println("\tpos: " + pos + " [" + j + "]\thgsv: '" + hgsv + "'\tEff: '" + hgsvEff + "'\t" + ceff.getEffectType());
+
+					// Is this an intron? (i.e. skip other effects, such as splice site)
+					// Compare expected to real HGSV strings
+					if (ceff.getEffectType() == EffectType.INTRON) Assert.assertEquals(hgsv, hgsvEff);
+				}
+			}
+
+			if (tested) checked++;
+		}
+		System.err.println("");
+	}
 
 	public void test_02() {
 		Gpr.debug("Test");
