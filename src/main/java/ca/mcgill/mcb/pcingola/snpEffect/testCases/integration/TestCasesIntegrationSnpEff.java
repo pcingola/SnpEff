6de873325c1df0c468f22b1fce27d99cbcad89aa
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-11-30 18:03:24 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
@@ -26,13 +26,6 @@ public class TestCasesIntegrationSnpEff extends IntegrationTest {
 	}
 
 	@Test
-	public void test_02() {
-		String expectedOutputFile = "tests/test.chr1.1line.out.txt";
-		String args[] = { "eff", "-v", "-o", "txt", "-noStats", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
-		command(new SnpEff(args), expectedOutputFile);
-	}
-
-	@Test
 	public void test_03() {
 		String expectedOutputFile = "tests/test.chr1.out.vcf.gz";
 		String args[] = { "eff", "-v", "-noStats", "testHg3763Chr1", "tests/test.chr1.vcf" };
