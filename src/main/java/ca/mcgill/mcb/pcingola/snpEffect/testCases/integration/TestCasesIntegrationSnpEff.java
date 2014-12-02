f1d4544db62f54206df3e562a7d57d6e12349cd2
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-02 09:27:13 -0500

    Integration test fixed

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
@@ -6,41 +6,41 @@ import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
 import ca.mcgill.mcb.pcingola.util.Gpr;
 
 /**
- * Invoke all integration test cases 
- * 
+ * Invoke all integration test cases
+ *
  * @author pcingola
  */
 public class TestCasesIntegrationSnpEff extends IntegrationTest {
 
-	//	@Test
-	//	public void test_01() {
-	//		Gpr.debug("Test");
-	//		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
-	//		String args[] = { "eff", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
-	//		command(new SnpEff(args), expectedOutputFile);
-	//	}
-	//
-	//	@Test
-	//	public void test_01_multi_thread() {
-	//		Gpr.debug("Test");
-	//		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
-	//		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
-	//		command(new SnpEff(args), expectedOutputFile);
-	//	}
-	//
-	//	@Test
-	//	public void test_03() {
-	//		Gpr.debug("Test");
-	//		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
-	//		String args[] = { "eff", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
-	//		command(new SnpEff(args), expectedOutputFile);
-	//	}
+	@Test
+	public void test_01() {
+		Gpr.debug("Test");
+		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
+		String args[] = { "eff", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
+		command(new SnpEff(args), expectedOutputFile);
+	}
+
+	@Test
+	public void test_01_multi_thread() {
+		Gpr.debug("Test");
+		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
+		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
+		command(new SnpEff(args), expectedOutputFile);
+	}
+
+	@Test
+	public void test_03() {
+		Gpr.debug("Test");
+		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
+		String args[] = { "eff", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
+		command(new SnpEff(args), expectedOutputFile);
+	}
 
 	@Test
 	public void test_03_multi_thread() {
 		Gpr.debug("Test");
 		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
-		String args[] = { "eff", "-v", "-t", "-classic", "-noStats", "-noLog", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
+		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
 		command(new SnpEff(args), expectedOutputFile);
 	}
 
