eddd4199b4a156add95a053d4dfd1f152ccc97f5
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-02 09:29:33 -0500

    Integration test multithreaded fixed

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEff.java
@@ -21,26 +21,10 @@ public class TestCasesIntegrationSnpEff extends IntegrationTest {
 	}
 
 	@Test
-	public void test_01_multi_thread() {
-		Gpr.debug("Test");
-		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
-		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
-		command(new SnpEff(args), expectedOutputFile);
-	}
-
-	@Test
-	public void test_03() {
+	public void test_02() {
 		Gpr.debug("Test");
 		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
 		String args[] = { "eff", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
-		command(new SnpEff(args), expectedOutputFile);
-	}
-
-	@Test
-	public void test_03_multi_thread() {
-		Gpr.debug("Test");
-		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
-		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
 		command(new SnpEff(args), expectedOutputFile);
 	}
 
--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEffMultiThread.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationSnpEffMultiThread.java
@@ -3,6 +3,7 @@ package ca.mcgill.mcb.pcingola.snpEffect.testCases.integration;
 import org.junit.Test;
 
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
+import ca.mcgill.mcb.pcingola.util.Gpr;
 
 /**
  * Invoke multi thread integration test
@@ -14,9 +15,18 @@ import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
 public class TestCasesIntegrationSnpEffMultiThread extends IntegrationTest {
 
 	@Test
-	public void test() {
-		String expectedOutputFile = "tests/test.chr1.out.multi-thread.vcf.gz";
-		String args[] = { "eff", "-v", "-t", "-noStats", "testHg3763Chr1", "tests/test.chr1.vcf" };
+	public void test_01_multi_thread() {
+		Gpr.debug("Test");
+		String expectedOutputFile = "tests/test.chr1.1line.out.classic.vcf";
+		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.1line.vcf" };
+		command(new SnpEff(args), expectedOutputFile);
+	}
+
+	@Test
+	public void test_02_multi_thread() {
+		Gpr.debug("Test");
+		String expectedOutputFile = "tests/test.chr1.eff.vcf.gz";
+		String args[] = { "eff", "-t", "-classic", "-noStats", "-noLog", "-noLof", "testHg3763Chr1", "tests/test.chr1.vcf.gz" };
 		command(new SnpEff(args), expectedOutputFile);
 	}
 
