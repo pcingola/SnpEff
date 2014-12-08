2756bf4da1032dca7e3e74442c3cc374ccf51075
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2014-12-08 15:14:20 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/TestCasesZzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/TestCasesZzz.java
@@ -1,12 +1,9 @@
 package ca.mcgill.mcb.pcingola.snpEffect.testCases;
 
-import org.junit.Assert;
 import org.junit.Test;
 
-import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
 import ca.mcgill.mcb.pcingola.snpEffect.testCases.unity.TestCasesBase;
 import ca.mcgill.mcb.pcingola.util.Gpr;
-import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 /**
  * Test case
@@ -18,38 +15,16 @@ public class TestCasesZzz extends TestCasesBase {
 	}
 
 	/**
-	 * Add and replace an INFO header
+	 * Some difficult HGVS test case
 	 */
 	@Test
-	public void test_28_vcfInfoReplace() {
+	public void test_zzz() {
 		Gpr.debug("Test");
 
-		verbose = true;
-		String vcfFileName = "tests/example_42.vcf";
+		String genome = "testHg19Chr1";
+		String vcf = "tests/hgvs_zzz.vcf";
 
-		// Replace all 'DP' fields using this value
-		String infoKey = "DP";
-		String infoValue = "42";
-
-		// Open VCF file
-		VcfFileIterator vcf = new VcfFileIterator(vcfFileName);
-		for (VcfEntry ve : vcf) {
-			String infoValuePrev = ve.getInfo(infoKey);
-
-			// Check that 'key=value' is in INFO
-			String keyValPrev = infoKey + "=" + infoValuePrev;
-			Assert.assertTrue("Old key=valu is not present", ve.getInfoStr().contains(keyValPrev));
-
-			// Replace value
-			ve.addInfo(infoKey, infoValue);
-			if (verbose) System.out.println(ve);
-
-			// Check that new 'key=value' is there
-			String keyVal = infoKey + "=" + infoValue;
-			Assert.assertTrue("New key=value is present", ve.toString().contains(keyVal));
-
-			// Check that previous 'key=value' is no longer there
-			Assert.assertTrue("Old key=value is still in INOF field", !ve.getInfoStr().contains(keyValPrev));
-		}
+		compareHgvs(genome, vcf);
 	}
+
 }
