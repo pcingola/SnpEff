f87be5c0e6d456d8aa141d77b4c66f888780fbee
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-07 22:03:42 -0500

    Project updated

--- scripts_build/examples.sh
+++ scripts_build/examples.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
 # Command lines from HTML examples
+#
+# Note: Sometimes we use testHg3775Chr1 instead of GRCh37.75 for speed ('testHg3775Chr1' only loads chr1 so it's faster)
+#
 
-java -Xmx4g -jar snpEff.jar -v -cancer -cancerSamples examples/samples_cancer_one.txt GRCh37.75 examples/cancer.vcf > examples/cancer.ann.vcf
+# Multiple annotations per variant
+java -Xmx4g -jar snpEff.jar testHg3775Chr1 examples/variants_1.vcf > examples/variants_1.ann.vcf
+
+java -Xmx4g -jar snpEff.jar testHg3775Chr1 examples/variants_2.vcf > examples/variants_2.ann.vcf
+
+# Cancer
+java -Xmx4g -jar snpEff.jar -v -cancer -cancerSamples examples/samples_cancer_one.txt testHg3775Chr1 examples/cancer.vcf > examples/cancer.ann.vcf
+
+java -Xmx4g -jar snpEff.jar -v -cancer testHg3775Chr1 examples/cancer_pedigree.vcf > examples/cancer_pedigree.ann.vcf
 
