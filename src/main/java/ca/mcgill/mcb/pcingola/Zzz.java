b1c74d894308f986b45e97885198d76444619361
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-01-28 01:00:58 -0300

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -3,7 +3,6 @@ package ca.mcgill.mcb.pcingola;
 import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
 import ca.mcgill.mcb.pcingola.snpEffect.commandLine.SnpEff;
 import ca.mcgill.mcb.pcingola.util.Gpr;
-import ca.mcgill.mcb.pcingola.vcf.VcfEffect;
 import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
 public class Zzz extends SnpEff {
@@ -12,28 +11,19 @@ public class Zzz extends SnpEff {
 		String vcfFile = Gpr.HOME + "/workspace/SnpEff/tests/test.chr1.eff.vcf";
 		VcfFileIterator vcf = new VcfFileIterator(vcfFile);
 		for (VcfEntry ve : vcf) {
+			// Show header (you may want to save it to a file instead)
 			if (vcf.isHeadeSection()) System.out.println(vcf.getVcfHeader());
 
+			//			// Query database
+			//
+			//			// Add annotations
+			//			VariantEffect variantEffect = new VariantEffect(variant, marker, effectType, effectImpact, message, codonsOld, codonsNew, codonNum, codonIndex, cDnaPos);
+			//			EffFormatVersion formatVersion = EffFormatVersion.FORMAT_ANN_1;
+			//			VcfEffect veff = new VcfEffect(variantEffect, formatVersion, true);
+			//			ve.addInfo(formatVersion.infoFieldName(), veff.toString());
+
+			// Show VCF line (you may want to save it to a file instead) 
 			System.out.println(ve);
-
-			StringBuilder effs = new StringBuilder();
-			for (VcfEffect veff : ve.parseEffects()) {
-				String gt = veff.getGenotype();
-				int gtIdx = Gpr.parseIntSafe(gt) - 1;
-
-				String gtNew = (gtIdx >= 0 ? ve.getAlts()[gtIdx] : gt);
-				if (gtNew.equals("null")) gtNew = null;
-
-				veff.setGenotype(gtNew);
-
-				if (effs.length() > 0) effs.append(",");
-				effs.append(veff.toString());
-
-				System.out.println("\t" + gt + "\t" + gtNew + "\t" + veff);
-			}
-
-			ve.addInfo("EFF", effs.toString());
-			System.out.println("\t=>" + ve);
 		}
 
 	}
