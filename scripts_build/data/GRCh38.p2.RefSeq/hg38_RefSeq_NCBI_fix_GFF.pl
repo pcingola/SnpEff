d9ae0f228e258b3a13f2cacca994b031df6d3401
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-04-11 11:58:38 -0400

    Project updated

--- scripts_build/data/GRCh38.p2.RefSeq/hg38_RefSeq_NCBI_fix_GFF.pl
+++ scripts_build/data/GRCh38.p2.RefSeq/hg38_RefSeq_NCBI_fix_GFF.pl
@@ -13,94 +13,153 @@ use strict;
 # Debug mode?
 my($debug) = 0;
 
+# Map chromosome IDs to names
+my(%chr);
+my(@keys) = ();		# GFF attribute key order
+
+#-------------------------------------------------------------------------------
+# Read an ID map file
+#-------------------------------------------------------------------------------
+sub readMap($) {
+	my($mapFile) = @_;
+	my($l, $name, $id, %ids);
+
+	open IDS, $mapFile || die "Cannot open chromosome ID map file '$mapFile'\n";
+	while( $l = <IDS> ) {
+		chomp $l;
+		($name, $id) = split /\s+/, $l;
+		$ids{$id} = $name;
+		print "MAP: ids{$id} = $name\n" if $debug;
+	}
+	close IDS;
+
+	return (%ids);
+}
+
+
+#-------------------------------------------------------------------------------
+# Parse GFF line
+#-------------------------------------------------------------------------------
+sub parseGffLine($) {
+	my($l) = @_;
+	my($chrid, $source, $type, $start, $end, $score, $strand, $phase, $attrs);
+
+	chomp $l;
+	($chrid, $source, $type, $start, $end, $score, $strand, $phase, $attrs) = split /\t/, $l;
+	print STDERR "Parsing GFF line:\t$chrid\t$source\t$type\t$start\t$end\t$score\t$strand\t$phase\t$attrs\n" if $debug;
+
+	#---
+	# Find chromosome name
+	#---
+	my($chr) = $chr{$chrid};
+	if( $chr eq '' ) {
+		print STDERR "WARNING: Cannot find chromosome name for id '$chrid'\n";
+		$chr{$chrid} = $chrid;
+	}
+
+	#---
+	# Translate types
+	#---
+	if( $type eq 'primary_transcript' ) { $type = 'transcript'; }
+
+	#---
+	# Parse attributes as key-value pairs
+	#---
+	my($key, $value, $kv, %attr, @kvs);
+	my(%attr) = ();
+	my(@kvs) = split /\;/, $attrs;
+	@keys = ();
+	foreach $kv ( @kvs ) {
+		($key, $value) = split /=/, $kv;
+		$attr{$key} = $value;
+		push( @keys, $key );
+		print STDERR "\tattr{$key} = $value\n" if $debug;
+	}
+
+	return ($chr, $source, $type, $start, $end, $score, $strand, $phase, \%attr);
+}
+
 #-------------------------------------------------------------------------------
 # Main
 #-------------------------------------------------------------------------------
 
 # Parse command line arguments
-my($chrMapFile) = $ARGV[0];
+die "Usage: $0 file.gff chromosome_ID_map_file.txt\n" if $#ARGV <= 0;
+my($gff) = $ARGV[0];
+my($chrMapFile) = $ARGV[1];
+die "Missing command line argument 'file.gff'\n" if $gff eq '';
 die "Missing command line argument 'chromosome_ID_map_file.txt'\n" if $chrMapFile eq '';
 
-#---
 # Read chromosome ID map file
-#---
-open CHR, $chrMapFile || die "Cannot open chromosome ID map file '$chrMapFile'\n";
-my($l, $chr, $id, %chr);
-while( $l = <CHR> ) {
-	chomp $l;
-	($chr, $id) = split /\s+/, $l;
-	$chr{$id} = $chr;
-	print "MAP: chr{$id} = $chr\n" if $debug;
-}
-close CHR;
+%chr = readMap($chrMapFile);
 
 #---
 # Parse GFF files from STDIN
 #---
-my($chr, $chrid, $source, $type, $start, $end, $score, $strand, $phase, $attrs, $id, $key, $value, $kv, $paren);
-my(%attr, @kvs, @keys, $name, %id, %trid, %prot);
-while( $l = <STDIN> ) {
 
-	if( $l =~ /^#/ ) {
-		# Show header / comment lines
-		print $l;
-	} else {
-		chomp $l;
-		($chrid, $source, $type, $start, $end, $score, $strand, $phase, $attrs) = split /\t/, $l;
-		print STDERR "ORI:\t$chr\t$source\t$type\t$start\t$end\t$score\t$strand\t$phase\t$attrs\n" if $debug;
+my($idsToChange) = {
+	'gene' => 1
+	,'C_gene_segment' => 1
+	,'D_gene_segment' => 1
+	,'J_gene_segment' => 1
+	,'V_gene_segment' => 1
+	, 'mRNA' => 1
+	, 'ncRNA' => 1
+	, 'primary_transcript' => 1
+	, 'rRNA' => 1
+	, 'tRNA' => 1
+	, 'transcript' => 1
+};
 
-		#---
-		# Find chromosome name
-		#---
-		$chr = $chr{$chrid};
-		if( $chr eq '' ) {
-			print STDERR "WARNING: Cannot find chromosome name for id '$chrid'\n";
-			$chr{$chrid} = $chrid;
+my($chr, $chrid, $source, $type, $start, $end, $score, $strand, $phase, $attr);	# GFF parsed values
+my(%name2id);	# Map name to IDs
+my(%idOld2New);	# Map old ID to new ID
+my(%trid);		# Map transcript ID to name
+my(%prot);		# Map protein ID to transcript ID
+my($l, $id, $key, $value, $name, $paren, $newParent);
+
+print STDERR "Parsing GFF file '$gff'\n";
+open GFF, $gff || die "Cannot open file '$gff'\n";
+while( $l = <GFF> ) {
+	# Skip headers
+	if( $l !~ /^#/ ) {
+		($chr, $source, $type, $start, $end, $score, $strand, $phase, $attr) = parseGffLine($l);
+
+		$name = $attr->{'Name'};
+		$id = $attr->{'ID'};
+		$paren = $attr->{'Parent'};
+
+		if( $idsToChange->{$type} ) {
+			if( $name ne '' ) {
+				# Name to ID map and create new unique ID using 'name'
+				if( $name2id{$name} ne '' ) {
+					$name2id{$name} .= "\t$id";
+					$idOld2New{$id} = "$name.$id"; # Append 'id' to make it unique
+            
+					print STDERR "Duplicated name '$name' using '$idOld2New{$id}'\n";
+				} else {
+					$idOld2New{$id} = $name;
+					$name2id{$name} = $id;
+				}
+			} else {
+				# No 'name' => Use same id
+				$idOld2New{$id} = $id;
+			}
+
+
+			# Replace ID
+			$id = $attr->{'ID'} = $idOld2New{$id};
 		}
-
-		#---
-		# Translate types
-		#---
-		if( $type eq 'primary_transcript' ) { $type = 'transcript'; }
-
-		#---
-		# Parse attributes as key-value pairs
-		#---
-		%attr = ();
-		@kvs = split /\;/, $attrs;
-		@keys = ();
-		foreach $kv ( @kvs ) {
-			($key, $value) = split /=/, $kv;
-			$attr{$key} = $value;
-			push( @keys, $key );
-			print STDERR "\tattr{$key} = $value\n" if $debug;
-		}
-
-		$name = $attr{'Name'};
-		$id = $attr{'ID'};
-		$paren = $attr{'Parent'};
-
-		#---
-		# Replace ID
-		#---
-		my($oldId, $newId);
-		$oldId = $attr{'ID'};
-		if( $attr{'Name'} ne '' )	{ $newId = $attr{'Name'} . "." . $oldId; }
-		else 						{ $newId = $oldId; }
-
-		$id{$oldId} = $newId;
-		$attr{'ID'} = $newId;
-		$trid{$newId} = $name if $name ne '';
 
 		#---
 		# Replace 'Parent'
 		#---
-		if( $attr{'Parent'} ne '' ) {
+		if( $paren ne '' ) {
 			my($oldParent, $newParent);
-			$oldParent = $attr{'Parent'};
-			$newParent = $id{$oldParent};
-			if( $newParent ne '' ) { $attr{'Parent'} = $newParent; }
-			else { print STDERR "WARNING: Cannot find ID for parent '$oldParent' in line: $l\n"; }
+			$oldParent = $paren;
+			$newParent = $idOld2New{$oldParent};
+			if( $newParent ne '' )	{ $attr->{'Parent'} = $newParent; }
+			else					{ print STDERR "WARNING: Cannot find ID for parent '$oldParent' in line: $l\n"; }
 
 			#---
 			# Protein - Transcript map
@@ -113,14 +172,13 @@ while( $l = <STDIN> ) {
 					$prot{$newParent} = $protId;
 				}
 			}
-
 		}
 
 		# Rebuild attributes
-		$attrs = "";
+		my($attrs) = "";
 		foreach $key ( @keys ) { 
 			$attrs .= ";" if $attrs ne ''; 
-			$attrs .= "$key=$attr{$key}";
+			$attrs .= "$key=" . $attr->{$key};
 		}
 
 		# Show GFF line
@@ -128,13 +186,15 @@ while( $l = <STDIN> ) {
 	}
 }
 
+close GFF;
+
 #---
 # Write IDs map file
 #---
 my($mapFile) = "ids.map.txt";
 print STDERR "Creating IDs transcript map file '$mapFile'\n";
 open IDS, "> $mapFile";
-foreach $key ( sort keys %id ) { print IDS "$key\t$id{$key}\n"; }
+foreach $key ( sort keys %idOld2New ) { print IDS "$key\t$idOld2New{$key}\n"; }
 close IDS;
 
 $mapFile = "protein_id.map.txt";
