a32e0c60abfcaf06282d1eb2d4058915b56806e3
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-05-01 10:49:59 -0400

    Project updated

--- scripts_build/data/Sitalica_164_v2.1/run.sh
+++ scripts_build/data/Sitalica_164_v2.1/run.sh
@@ -5,7 +5,7 @@ cp $HOME/Downloads/Sitalica_164_v2.1.protein protein.fa
 cp $HOME/Downloads/Sitalica_164_v2 ../genomes/Sitalica_164_v2.1.fa
 
 # Fix CDS names in GFF file (there are some errors in the GFF)
-cat $HOME/Downloads/Sitalica_164_v2.1.gene genes.gff \
+cat $HOME/Downloads/Sitalica_164_v2.1.gene \
     | sed "s/\.version2.1.CDS.[0-9]*/.CDS/" \
     | sed "s/\.version2.1//" \
     > genes.gff
