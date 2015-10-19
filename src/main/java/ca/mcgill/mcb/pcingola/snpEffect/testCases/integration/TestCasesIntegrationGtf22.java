309f97493ab63da3196921a89ca4bf645d044fc3
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-10-18 23:29:11 -0400

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationGtf22.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/testCases/integration/TestCasesIntegrationGtf22.java
@@ -32,6 +32,35 @@ public class TestCasesIntegrationGtf22 extends TestCasesIntegrationBase {
 	}
 
 	@Test
+	public void testCaseHg37_61_ENST00000250838() {
+		Gpr.debug("Test");
+		String genome = "testHg37.61";
+		String gtfFile = "tests/ENST00000250838.gtf";
+		String fastaFile = "tests/chrY.fa.gz";
+		String resultFile = "tests/ENST00000250838.txt";
+		buildGtfAndCompare(genome, gtfFile, fastaFile, resultFile);
+	}
+
+	@Test
+	public void testCaseHg37_61_ENST00000331397() {
+		Gpr.debug("Test");
+		String genome = "testHg37.61";
+		String gtfFile = "tests/ENST00000331397.gtf22";
+		String fastaFile = "tests/chrY.fa.gz";
+		String resultFile = "tests/ENST00000331397.txt";
+		buildGtfAndCompare(genome, gtfFile, fastaFile, resultFile);
+	}
+
+	@Test
+	public void testCaseMm37_61_ENSMUSG00000051951() {
+		Gpr.debug("Test");
+		String genome = "testMm37.61";
+		String gtfFile = "tests/ENSMUSG00000051951.gtf";
+		String resultFile = "tests/ENSMUSG00000051951.txt";
+		buildGtfAndCompare(genome, gtfFile, null, resultFile);
+	}
+
+	@Test
 	public void testCaseMm37_61_ENSMUST00000070533() {
 		Gpr.debug("Test");
 		String genome = "testMm37.61";
