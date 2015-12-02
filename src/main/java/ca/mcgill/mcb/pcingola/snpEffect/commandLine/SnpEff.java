887c54101a57ad29bdacc2465bc33ffe1b0ce972
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-12-02 10:38:28 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/snpEffect/commandLine/SnpEff.java
+++ src/main/java/ca/mcgill/mcb/pcingola/snpEffect/commandLine/SnpEff.java
@@ -73,7 +73,7 @@ public class SnpEff implements CommandLine {
 	// Version info
 	public static final String SOFTWARE_NAME = "SnpEff";
 	public static final String REVISION = "";
-	public static final String BUILD = "2015-12-01";
+	public static final String BUILD = "2015-12-02";
 	public static final String VERSION_MAJOR = "4.2";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 	public static final String VERSION_NO_NAME = VERSION_SHORT + " (build " + BUILD + "), by " + Pcingola.BY;
