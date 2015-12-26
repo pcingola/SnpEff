7a1ec6d364e0dd0b5a818879119894ea88cd1323
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2015-12-26 09:39:14 -0500

    Project updated

--- src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
+++ src/main/java/ca/mcgill/mcb/pcingola/Zzz.java
@@ -1,22 +1,15 @@
 package ca.mcgill.mcb.pcingola;
 
-import ca.mcgill.mcb.pcingola.fileIterator.VcfFileIterator;
-import ca.mcgill.mcb.pcingola.interval.Variant;
 import ca.mcgill.mcb.pcingola.util.Gpr;
-import ca.mcgill.mcb.pcingola.vcf.VcfEntry;
 
+/**
+ * Sort SAM entries using genomic coordinates 
+ */
 public class Zzz {
 
 	public static void main(String[] args) {
-		String vcfFileName = Gpr.HOME + "/snpEff/z.vcf";
-		// String vcfFileName = Gpr.HOME + "/snpEff/miika_FGFR3_TACC3_fusion.vcf";
+		String fileName = Gpr.HOME + "/snpEff/z.vcf";
 
-		VcfFileIterator vcf = new VcfFileIterator(vcfFileName);
-		for (VcfEntry ve : vcf) {
-			System.out.println(ve);
-			for (Variant v : ve.variants())
-				System.out.println("\t" + v.isVariant() + "\t" + v);
-		}
 	}
 
 }
