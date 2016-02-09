c3a21ec552e5c6ae3a1b0a9f4a9c6599409910d4
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-02-09 14:27:13 -0500

    Project updated

--- src/main/java/org/snpeff/Zzz.java
+++ src/main/java/org/snpeff/Zzz.java
@@ -1,15 +1,42 @@
 package org.snpeff;
 
+import org.snpeff.fileIterator.VcfFileIterator;
 import org.snpeff.util.Gpr;
+import org.snpeff.vcf.VcfEffect;
+import org.snpeff.vcf.VcfEntry;
 
 /**
- * Sort SAM entries using genomic coordinates 
+ * 
  */
 public class Zzz {
 
 	public static void main(String[] args) {
-		String fileName = Gpr.HOME + "/snpEff/z.vcf";
+		String vcfFileName = Gpr.HOME + "/snpEff/z.ann.vcf";
 
+		VcfFileIterator vcf = new VcfFileIterator(vcfFileName);
+
+		// Each VCF line is a VcfEntry
+		for (VcfEntry vcfEntry : vcf) {
+			System.out.println(vcfEntry);
+
+			// Parse annotations 'ANN' (formerly known as effects 'EFF')
+			for (VcfEffect veff : vcfEntry.getVcfEffects()) {
+				System.out.println("\t" + veff);
+				System.out.println("\t\tAlele: " + veff.getAllele());
+				System.out.println("\t\tEffects: " + veff.getEffectTypesStr());
+				System.out.println("\t\tTranscript ID: " + veff.getFeatureId());
+				System.out.println("\t\tHGVS.c: " + veff.getHgvsC());
+				System.out.println("\t\tHGVS.p: " + veff.getHgvsP());
+			}
+
+			// TODO: Add 'auto curate'
+			// Format:
+			//		Reasons why the variant is filtered: {NON_CODING, AF_1KG, ...., AF_EXAC, ... } => Mapped to some codes 
+			//      One auto-curate code per variant annotation entry (VcfEffect)
+			//      AUTO_CURATE_CODE=5,2,4,0,...,12 
+			// vcfEntry.getInfo("AUTO_CURATE");
+
+		}
 	}
 
 }
