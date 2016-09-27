c56e5cc0ced961e529d2a93ee53ed21ea746508a
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-09-27 06:13:43 -0400

    Project updated

--- src/main/java/org/snpeff/interval/GffMarker.java
+++ src/main/java/org/snpeff/interval/GffMarker.java
@@ -238,7 +238,6 @@ public class GffMarker extends Custom {
 	 */
 	protected void parse(Genome genome, String line) {
 		// Split fields
-		Gpr.debug("line: " + line);
 		String fields[] = line.split("\t");
 
 		// Parse chromosome
