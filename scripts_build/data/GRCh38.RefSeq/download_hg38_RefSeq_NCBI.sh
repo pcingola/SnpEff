7a440dca63e0a2e0b257a86eb90e05e040945c6f
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-10-30 07:51:53 -0400

    Project updated

---
+++ scripts_build/data/GRCh38.RefSeq/download_hg38_RefSeq_NCBI.sh
@@ -0,0 +1,83 @@
+#!/bin/sh -e
+
+# Genome name (in SnpEff's config file)
+VER="p7"
+GENOME="GRCh38.$VER.RefSeq"
+
+# Path to scripts
+SNPEFF_DIR="$HOME/snpEff"
+SCRIPTS_DIR="$SNPEFF_DIR/scripts"
+SCRIPTS_BUILD_DIR="$SNPEFF_DIR/scripts_build/data/GRCh38.RefSeq"
+DB_DIR="$SNPEFF_DIR/data/$GENOME"
+
+# File names
+GFF_REF="ref_GRCh38.$VER""_top_level.gff3.gz"
+CHR_IDS="$DB_DIR/chromosomes.txt"
+CHR_IDS_2_NAME="$DB_DIR/chromosomes2name.txt"
+
+# Create dir
+mkdir -p $DB_DIR/ORI
+cd $DB_DIR/ORI
+
+#---
+# Download FASTA files
+#---
+
+echo Downloading reference seuquences
+wget -N "ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/H_sapiens/Assembled_chromosomes/seq/hs_ref_GRCh38.$VER\_*.fa.gz"
+
+echo Downloading mRNA sequences
+wget -N "ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.*.rna.fna.gz"
+
+echo Downloading protein sequences
+wget -N "ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/mRNA_Prot/human.*.protein.faa.gz"
+
+#---
+# Download GFF data
+#---
+
+echo "Download GFF files"
+wget -N ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/H_sapiens/GFF/$GFF_REF
+
+echo "Copying genes.ORI.gff file"
+cp $GFF_REF genes.ORI.gff.gz
+gunzip -c genes.ORI.gff.gz > genes.ORI.gff
+
+#---
+# Download chromosome IDs file
+#---
+
+ASSEMBLY_ID=`zcat genes.ORI.gff.gz | head -n 100 | grep "^#" | grep genome-build-accession | cut -f 2 -d :`
+echo "Assembly ID: $ASSEMBLY_ID"
+
+echo "Download chromosome IDs map file"
+wget -O - ftp://ftp.ncbi.nlm.nih.gov/genomes/ASSEMBLY_REPORTS/All/$ASSEMBLY_ID.assembly.txt > $CHR_IDS
+
+echo "Create a file mapping chromosome IDs to names: $CHR_IDS_2_NAME"
+cat $CHR_IDS | cut -f 1,7 | grep -v "^#" > $CHR_IDS_2_NAME
+
+#---
+# Process files
+#---
+
+echo "Processing GFF file"
+$SCRIPTS_BUILD_DIR/hg38_RefSeq_NCBI_fix_GFF.pl genes.ORI.gff $CHR_IDS_2_NAME > $DB_DIR/genes.gff 
+gzip $DB_DIR/genes.gff
+
+echo "Processing reference FASTA files"
+gunzip -c hs_ref*.fa.gz | $SCRIPTS_BUILD_DIR/hg38_RefSeq_NCBI_fix_FASTA.pl $CHR_IDS_2_NAME | $SCRIPTS_DIR/fastaSplit.pl 
+
+echo "Creating and compressing FASTA file $GENOME.fa"
+cat chr[1-9].fa chr[1,2][0-9].fa chrX.fa chrY.fa chrMT.fa chr???*.fa | gzip -c > $DB_DIR/$GENOME.fa.gz
+
+echo "Processing protein FASTA files"
+gunzip -c human.?.protein.faa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > protein.ORI.fa 
+cat protein.ORI.fa | $SCRIPTS_BUILD_DIR/hg38_RefSeq_NCBI_fix_FASTA_ProCds.pl protein_id.map.txt > $DB_DIR/protein.fa
+gzip $DB_DIR/protein.fa
+
+echo "Processing RNA FASTA files"
+gunzip -c human.?.rna.fna.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > cds.ORI.fa 
+cat cds.ORI.fa | $SCRIPTS_BUILD_DIR/hg38_RefSeq_NCBI_fix_FASTA_ProCds.pl ids.map.txt > $DB_DIR/cds.fa
+gzip $DB_DIR/cds.fa
+
+echo "Done."
