597d380115752b9d24b04962efbc7ad0de02e874
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2016-11-15 12:57:23 -0500

    Project updated

--- src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz.java
+++ src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz.java
@@ -1,5 +1,13 @@
 package org.snpeff.snpEffect.testCases.integration;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.snpeff.interval.Gene;
+import org.snpeff.interval.Genome;
+import org.snpeff.interval.Transcript;
+import org.snpeff.snpEffect.SnpEffectPredictor;
+import org.snpeff.util.Gpr;
+
 /**
  *
  * Test cases for variants
@@ -10,6 +18,37 @@ public class TestCasesIntegrationZzz extends TestCasesIntegrationBase {
 
 	public TestCasesIntegrationZzz() {
 		super();
+	}
+
+	@Test
+	public void testCase_06_CircularGenome_ExonsOrder() {
+		Gpr.debug("Test");
+
+		String expectedProtein = "MALQTDTQAWRVEIGTRGLMFSNCVPLHLPEGQYHKLRLPVSAY" // 
+				+ "EALAVARYGLVGSLWEVPAVNSALQCLAAAAPCKDVKIYPSCIFQVHAPMFVTIKTSL" // 
+				+ "RCLNPHDLCLCLICVGAAILDIPLLCAPRDGAGARAAEGQAAAAQGGKLRVWGRLSPS" // 
+				+ "SPTSLSLAFPYAGPPPVAWYRHSINLTRSEGVGIGKDCAQDHACPVPPQGHASSAADQ" // 
+				+ "AGVPERGRKRAHEGPGAGEAASAGRGDVALSQSRALLWRGLGWDTGRGRLAPGLAMSR" // 
+				+ "DAASGSVHLDIQVDRAEEGWVCDVLLEPGPPTAREGCSLSMDPGLVTLKDAWTLFPLH" // 
+				+ "PEHDAVVPPKEEIHVMAQGHLQGGTPSLWGFTFQEAACDQWVLRPRVWTAHSPIKMTV" // 
+				+ "YNCGHKPLHIGPSTRLGLALFWPAERSDNLDAGRIFYQLTSGELYWGRTVARPPTLTL" // 
+				+ "PVDELRPWPKLTPEEPMQH*" //
+		;
+
+		// Create database & build interval forest
+		String genomeName = "testCase";
+		String genBankFile = "tests/Human_herpesvirus_4_uid14413.gbk";
+		SnpEffectPredictor sep = buildGeneBank(genomeName, genBankFile);
+		sep.buildForest();
+
+		// Create variant
+		Genome genome = sep.getGenome();
+		Gene gene = genome.getGenes().getGeneByName("LF1");
+		Transcript tr = gene.iterator().next();
+		String prot = tr.protein();
+
+		if (verbose) Gpr.debug("Transcript: " + tr);
+		Assert.assertEquals("Protein sequence does not match", expectedProtein, prot);
 	}
 
 }
