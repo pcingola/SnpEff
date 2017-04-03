17a953553926496da3d5c6b22de224121015e2a4
Pablo Cingolani <pablo.e.cinglani@gmail.com>
2017-04-03 15:30:36 -0400

    Project updated

--- src/main/java/org/snpeff/SnpEff.java
+++ src/main/java/org/snpeff/SnpEff.java
@@ -77,10 +77,10 @@ public class SnpEff implements CommandLine {
 
 	// Version info
 	public static final String SOFTWARE_NAME = "SnpEff";
-	public static final String REVISION = "k";
+	public static final String REVISION = "";
 	public static final String BUILD = Gpr.compileTimeStamp(SnpEff.class);
 	public static final String BUILD_DATE = Gpr.compileDate(SnpEff.class);
-	public static final String VERSION_MAJOR = "4.3";
+	public static final String VERSION_MAJOR = "5.0";
 	public static final String VERSION_SHORT = VERSION_MAJOR + REVISION;
 	public static final String VERSION_BUILD = VERSION_SHORT + " (build " + BUILD + ")";
 	public static final String VERSION_AUTHOR = VERSION_BUILD + ", by " + Pcingola.BY;
