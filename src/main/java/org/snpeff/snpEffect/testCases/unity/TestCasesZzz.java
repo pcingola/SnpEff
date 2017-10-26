ce90066cdd974d6852001d3fc4bc07d72c0f33ad
Pablo Cingolani <pablo.e.cinglani@gmail.com>
2017-10-26 11:58:01 -0400

    Project updated

--- src/main/java/org/snpeff/snpEffect/testCases/unity/TestCasesZzz.java
+++ src/main/java/org/snpeff/snpEffect/testCases/unity/TestCasesZzz.java
@@ -1,12 +1,8 @@
 package org.snpeff.snpEffect.testCases.unity;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.Test;
-import org.snpeff.SnpEff;
-import org.snpeff.snpEffect.commandLine.SnpEffCmdEff;
 import org.snpeff.snpEffect.testCases.integration.TestCasesIntegrationBase;
 import org.snpeff.util.Gpr;
 import org.snpeff.vcf.VcfEffect;
@@ -25,53 +21,53 @@ public class TestCasesZzz extends TestCasesIntegrationBase {
 
 	public TestCasesZzz() {
 		super();
-		testsDir = "tests/integration/hgvs/hgvs_up_down_stream";
+		testsDir = "tests/integration/hgvsUpDownStream/";
 	}
 
-	/**
-	 * Check that RAW alt fields are kept in 'Allele/Genotype'
-	 */
-	@Test
-	public void test_01_VcfRawAlt() {
-		Gpr.debug("Test");
-		verbose = true;
-
-		// Create command
-		String args[] = { "testHg3775Chr1", "tests/test_ann_integration_01.vcf" };
-
-		SnpEff cmd = new SnpEff(args);
-		SnpEffCmdEff cmdEff = (SnpEffCmdEff) cmd.cmd();
-		cmdEff.setVerbose(verbose);
-		cmdEff.setSupressOutput(!verbose);
-
-		// Run command
-		List<VcfEntry> list = cmdEff.run(true);
-		Assert.assertTrue("Errors while executing SnpEff", cmdEff.getTotalErrs() <= 0);
-
-		// Expected results
-		Set<String> allelesExpected = new HashSet<>();
-		allelesExpected.add("AACACACACACACACACACACACACACACACACACACAC");
-		allelesExpected.add("AACACACACACACACACACACACACACACACACACACACAC");
-		allelesExpected.add("AACACACACACACACACACACACACACAC");
-		allelesExpected.add("AACACACACACACACACACACACACACACACACACACACACAC");
-		allelesExpected.add("AACACACACACACACACACACACACACACACACACAC");
-
-		// Find AA change for a genotype
-		Set<String> allelesReal = new HashSet<>();
-		for (VcfEntry vcfEntry : list) {
-			if (debug) System.err.println(vcfEntry);
-
-			for (VcfEffect eff : vcfEntry.getVcfEffects()) {
-				String allele = eff.getAllele();
-				if (verbose) System.err.println("\t" + eff + "\n\t\tAllele: " + allele);
-
-				Assert.assertTrue("Unexpected allele '" + allele + "'", allelesExpected.contains(allele));
-				allelesReal.add(allele);
-			}
-		}
-
-		Assert.assertEquals(allelesExpected, allelesReal);
-	}
+	//	/**
+	//	 * Check that RAW alt fields are kept in 'Allele/Genotype'
+	//	 */
+	//	@Test
+	//	public void test_01_VcfRawAlt() {
+	//		Gpr.debug("Test");
+	//		verbose = true;
+	//
+	//		// Create command
+	//		String args[] = { "testHg3775Chr1", "tests/test_ann_integration_01.vcf" };
+	//
+	//		SnpEff cmd = new SnpEff(args);
+	//		SnpEffCmdEff cmdEff = (SnpEffCmdEff) cmd.cmd();
+	//		cmdEff.setVerbose(verbose);
+	//		cmdEff.setSupressOutput(!verbose);
+	//
+	//		// Run command
+	//		List<VcfEntry> list = cmdEff.run(true);
+	//		Assert.assertTrue("Errors while executing SnpEff", cmdEff.getTotalErrs() <= 0);
+	//
+	//		// Expected results
+	//		Set<String> allelesExpected = new HashSet<>();
+	//		allelesExpected.add("AACACACACACACACACACACACACACACACACACACAC");
+	//		allelesExpected.add("AACACACACACACACACACACACACACACACACACACACAC");
+	//		allelesExpected.add("AACACACACACACACACACACACACACAC");
+	//		allelesExpected.add("AACACACACACACACACACACACACACACACACACACACACAC");
+	//		allelesExpected.add("AACACACACACACACACACACACACACACACACACAC");
+	//
+	//		// Find AA change for a genotype
+	//		Set<String> allelesReal = new HashSet<>();
+	//		for (VcfEntry vcfEntry : list) {
+	//			if (debug) System.err.println(vcfEntry);
+	//
+	//			for (VcfEffect eff : vcfEntry.getVcfEffects()) {
+	//				String allele = eff.getAllele();
+	//				if (verbose) System.err.println("\t" + eff + "\n\t\tAllele: " + allele);
+	//
+	//				Assert.assertTrue("Unexpected allele '" + allele + "'", allelesExpected.contains(allele));
+	//				allelesReal.add(allele);
+	//			}
+	//		}
+	//
+	//		Assert.assertEquals(allelesExpected, allelesReal);
+	//	}
 
 	/**
 	 * Test HGVS upstream of a variant affecting a transcript on the negative strand
