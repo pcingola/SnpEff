f4d36c321638fa6f979325cdc31ec012a24fb98f
Pablo Cnigolani <pablo.e.cingolani@gmail.com>
2020-01-21 19:00:27 -0500

    Project updated

--- src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationRefSeq.java
+++ src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationRefSeq.java
@@ -5,6 +5,11 @@ import java.util.HashMap;
 import org.junit.Test;
 import org.snpeff.SnpEff;
 import org.snpeff.align.SmithWaterman;
+import org.snpeff.interval.Variant;
+import org.snpeff.snpEffect.EffectType;
+import org.snpeff.snpEffect.SnpEffectPredictor;
+import org.snpeff.snpEffect.VariantEffect;
+import org.snpeff.snpEffect.VariantEffects;
 import org.snpeff.snpEffect.commandLine.SnpEffCmdBuild;
 import org.snpeff.util.Gpr;
 
@@ -21,26 +26,26 @@ public class TestCasesIntegrationRefSeq extends TestCasesIntegrationBase {
 		super();
 	}
 
-	//	@Test
-	//	public void test_01() {
-	//		Gpr.debug("Test");
-	//
-	//		/// Build SnpEffectPredictor using a RefSeq file
-	//		String genome = "hg19";
-	//		String refSeqFile = path("hg19_refSeq_OR4F16.txt");
-	//		String fastaFile = null;
-	//		String resultFile = path("hg19_refSeq_OR4F16.dump.txt");
-	//		SnpEffectPredictor sep = buildAndCompare(genome, refSeqFile, fastaFile, resultFile, true);
-	//
-	//		// Check a SNP
-	//		sep.buildForest();
-	//		Variant variant = new Variant(sep.getGenome().getChromosome("1"), 521603, "A", "G");
-	//		VariantEffects effs = sep.variantEffect(variant);
-	//		for (VariantEffect eff : effs) {
-	//			if (verbose) System.out.println("\t" + eff);
-	//			Assert.assertEquals(eff.getEffectType(), EffectType.INTERGENIC);
-	//		}
-	//	}
+	@Test
+	public void test_01() {
+		Gpr.debug("Test");
+
+		/// Build SnpEffectPredictor using a RefSeq file
+		String genome = "hg19";
+		String refSeqFile = path("hg19_refSeq_OR4F16.txt");
+		String fastaFile = null;
+		String resultFile = path("hg19_refSeq_OR4F16.dump.txt");
+		SnpEffectPredictor sep = buildAndCompare(genome, refSeqFile, fastaFile, resultFile, true);
+
+		// Check a SNP
+		sep.buildForest();
+		Variant variant = new Variant(sep.getGenome().getChromosome("1"), 521603, "A", "G");
+		VariantEffects effs = sep.variantEffect(variant);
+		for (VariantEffect eff : effs) {
+			if (verbose) System.out.println("\t" + eff);
+			Assert.assertEquals(eff.getEffectType(), EffectType.INTERGENIC);
+		}
+	}
 
 	/**
 	 * Test improved exon frame correction in UCSC references
@@ -48,7 +53,6 @@ public class TestCasesIntegrationRefSeq extends TestCasesIntegrationBase {
 	@Test
 	public void test_02() {
 		Gpr.debug("Test");
-		verbose = true;
 
 		/// Build SnpEffectPredictor using a RefSeq file
 		String genome = "testNM_015296";
