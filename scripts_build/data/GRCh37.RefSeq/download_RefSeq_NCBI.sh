c15a80ee12e27089b22b06d3bda94cf1091178db
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-01-27 14:25:47 -0500

    Project updated

--- scripts_build/data/GRCh37.RefSeq/download_RefSeq_NCBI.sh
+++ scripts_build/data/GRCh37.RefSeq/download_RefSeq_NCBI.sh
@@ -13,8 +13,8 @@ SCRIPTS_BUILD_DIR="$SNPEFF_DIR/data/$VER.$SUBVER.RefSeq"
 DB_DIR="$SNPEFF_DIR/data/$GENOME"
 
 # File names
-ARCHIVE="ARCHIVE/ANNOTATION_RELEASE.105"
-FTP_DIR="ftp://ftp.ncbi.nih.gov/genomes/Homo_sapiens/$ARCHIVE"
+ARCHIVE="ARCHIVE/ANNOTATION_RELEASE.109"
+HTTP_DIR="http://ftp.ncbi.nih.gov/genomes/Homo_sapiens/$ARCHIVE"
 GFF_REF="ref_$VER.$SUBVER""_top_level.gff3.gz"
 CHR_IDS="$DB_DIR/chromosomes.txt"
 CHR_IDS_2_NAME="$DB_DIR/chromosomes2name.txt"
@@ -28,13 +28,13 @@ cd $DB_DIR/ORI
 #---
 
 echo Downloading reference sequences
-wget -N "$FTP_DIR/Assembled_chromosomes/seq/hs_ref_$VER.$SUBVER\_chr*.fa.gz"
- 
+wget -N "$HTTP_DIR/Assembled_chromosomes/seq/hs_ref_$VER.$SUBVER\_chr*.fa.gz"
+
 echo Downloading mRNA sequences
-wget -N "$FTP_DIR/RNA/rna.fa.gz"
+wget -N "$HTTP_DIR/RNA/rna.fa.gz"
 
 echo Downloading protein sequences
-wget -N "$FTP_DIR/protein/protein.fa.gz"
+wget -N "$HTTP_DIR/protein/protein.fa.gz"
 
 #---
 # Download GFF data
@@ -68,23 +68,23 @@ cat $CHR_IDS | cut -f 1,7 | grep -v "^#" > $CHR_IDS_2_NAME
 #---
 
 echo "Processing GFF file"
-$SCRIPTS_BUILD_DIR/fix_gff.pl genes.ORI.gff $CHR_IDS_2_NAME > $DB_DIR/genes.gff 
+$SCRIPTS_BUILD_DIR/fix_gff.pl genes.ORI.gff $CHR_IDS_2_NAME > $DB_DIR/genes.gff
 gzip $DB_DIR/genes.gff
 
 echo "Processing reference FASTA files"
-gunzip -c hs_ref*.fa.gz | $SCRIPTS_BUILD_DIR/fix_fasta.pl $CHR_IDS_2_NAME | $SCRIPTS_DIR/fastaSplit.pl 
+gunzip -c hs_ref*.fa.gz | $SCRIPTS_BUILD_DIR/fix_fasta.pl $CHR_IDS_2_NAME | $SCRIPTS_DIR/fastaSplit.pl
 
 echo "Creating and compressing FASTA file $GENOME.fa.gz"
 cat chr[1-9].fa chr[1,2][0-9].fa chrX.fa chrY.fa chrMT.fa chr???*.fa | gzip -c > $DB_DIR/sequences.fa.gz
 
 echo "Processing protein FASTA files"
-gunzip -c protein.fa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > protein.ORI.fa 
+gunzip -c protein.fa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > protein.ORI.fa
 cat protein.ORI.fa | $SCRIPTS_BUILD_DIR/fix_fasta_protein_cds.pl protein_id.map.txt > $DB_DIR/protein.fa
 gzip -f $DB_DIR/protein.fa
 
 echo "Processing RNA FASTA files"
 gunzip -c rna.fa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" | gzip -c > $DB_DIR/cds.fa.gz
-#gunzip -c rna.fa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > cds.ORI.fa 
+#gunzip -c rna.fa.gz | sed "s/^>gi|[0-9]*|ref|\(.*\)|.*/>\1/" > cds.ORI.fa
 #cat cds.ORI.fa | $SCRIPTS_BUILD_DIR/fix_fasta_protein_cds.pl ids.map.txt > $DB_DIR/cds.fa
 #gzip -f $DB_DIR/cds.fa
 
