72d58633055d39be9f4f732abb0935e19d6ae29c
Pablo Cnigolani <pablo.e.cingolani@gmail.com>
2020-04-15 20:00:07 -0400

    Project updated

--- src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz.java
+++ src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz.java
@@ -1,11 +1,8 @@
 package org.snpeff.snpEffect.testCases.integration;
 
 import org.junit.Test;
-import org.snpeff.snpEffect.commandLine.SnpEffCmdBuild;
-import org.snpeff.snpEffect.commandLine.SnpEffCmdProtein;
+import org.snpeff.snpEffect.VariantEffect.EffectImpact;
 import org.snpeff.util.Gpr;
-
-import junit.framework.Assert;
 
 /**
  * Test case
@@ -19,34 +16,34 @@ public class TestCasesIntegrationZzz extends TestCasesIntegrationBase {
 		super();
 	}
 
-	@Test
-	public void test_01() {
-		Gpr.debug("Test");
-		String genome = "test_NC_045512_01";
-		SnpEffCmdBuild buildCmd = buildGetBuildCmd(genome);
+	//	@Test
+	//	public void test_01() {
+	//		Gpr.debug("Test");
+	//		String genome = "test_NC_045512_01";
+	//		SnpEffCmdBuild buildCmd = buildGetBuildCmd(genome);
+	//
+	//		// Make sure all proteins are OK
+	//		SnpEffCmdProtein protCmd = buildCmd.getSnpEffCmdProtein();
+	//		Assert.assertEquals(2, protCmd.getTotalOk());
+	//		Assert.assertEquals(0, protCmd.getTotalErrors());
+	//		Assert.assertEquals(0, protCmd.getTotalWarnings());
+	//		Assert.assertEquals(0, protCmd.getTotalNotFound());
+	//	}
 
-		// Make sure all proteins are OK
-		SnpEffCmdProtein protCmd = buildCmd.getSnpEffCmdProtein();
-		Assert.assertEquals(2, protCmd.getTotalOk());
-		Assert.assertEquals(0, protCmd.getTotalErrors());
-		Assert.assertEquals(0, protCmd.getTotalWarnings());
-		Assert.assertEquals(0, protCmd.getTotalNotFound());
+	@Test
+	public void test_02_ann() {
+		Gpr.debug("Test");
+		// Note: Normally this EffectImpact should be 'HIGH' impact, but
+		// since the database we build in test_01_build is small, there
+		// are not enough stats.
+		checkNextProt("testHg3770Chr22" //
+				, path("test_nextProt_02.vcf")//
+				, "amino_acid_modification:N-acetylglycine"//
+				, EffectImpact.LOW //
+				, true //
+		);
 	}
 
-	//	@Test
-	//	public void test_02_ann() {
-	//		Gpr.debug("Test");
-	//		// Note: Normally this EffectImpact should be 'HIGH' impact, but
-	//		// since the database we build in test_01_build is small, there
-	//		// are not enough stats.
-	//		checkNextProt("testHg3770Chr22" //
-	//				, path("test_nextProt_02.vcf")//
-	//				, "amino_acid_modification:N-acetylglycine"//
-	//				, EffectImpact.LOW //
-	//				, true //
-	//		);
-	//	}
-	//
 	//	@Test
 	//	public void test_03_ann() {
 	//		Gpr.debug("Test");
