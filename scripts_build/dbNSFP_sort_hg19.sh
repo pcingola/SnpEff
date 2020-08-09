3ad186376514a58cd5b55c38d774733c05cc94a5
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2020-08-09 16:33:19 -0400

    Project updated

--- scripts_build/dbNSFP_sort_hg19.sh
+++ scripts_build/dbNSFP_sort_hg19.sh
@@ -3,8 +3,6 @@ set -o pipefail
 
 # Scripts dir
 scripts=$(cd $(dirname "$0"); pwd -P)
-echo "0='$0'"
-echo "SCRIPTS='$scripts'"
 
 # Change this 
 version="4.1a"
@@ -17,7 +15,7 @@ prefix_input="dbNSFP${version}_variant.chr"
 prefix_split="dbNSFP.split"
 output="dbNSFP${version}.sort_hg19.txt"
 
-echo "Splitting input file '$input' by chromosome"
+echo "Splitting input files '$prefix_input' by chromosome"
 cat $prefix_input* \
 	| $scripts/dbNSFP_split_by_chr.pl 7 8 \
 
