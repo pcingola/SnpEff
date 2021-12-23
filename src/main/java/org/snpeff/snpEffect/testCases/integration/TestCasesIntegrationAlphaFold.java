a7b0ff93af8a20b687e6563cc88bcc671d5859ac
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2021-12-23 15:40:25 -0500

    Project updated

---
+++ src/main/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationAlphaFold.java
@@ -0,0 +1,16 @@
+package org.snpeff.snpEffect.testCases.integration;
+
+/**
+ * Test case
+ */
+public class TestCasesIntegrationAlphaFold extends TestCasesIntegrationBase {
+
+    long randSeed = 20100629;
+    String genomeName = "testCase";
+
+    public TestCasesIntegrationAlphaFold() {
+        super();
+    }
+
+
+}
