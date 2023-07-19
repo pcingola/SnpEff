2e76ff933d53eb8863b03539372bda98d9fb8a15
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2023-07-19 12:12:44 -0400

    Project updated

--- src/main/java/org/snpeff/SnpEff.java
+++ src/main/java/org/snpeff/SnpEff.java
@@ -3,7 +3,6 @@ package org.snpeff;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
