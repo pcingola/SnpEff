684359fea8de3292f584381179038857e0ec33f9
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2023-11-15 16:23:16 -0500

    Project updated

--- src/test/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz2.java
+++ src/test/java/org/snpeff/snpEffect/testCases/integration/TestCasesIntegrationZzz2.java
@@ -12,32 +12,33 @@ import org.snpeff.interval.Transcript;
  */
 public class TestCasesIntegrationZzz2 extends TestCasesIntegrationBase {
 
-    /**
-     * Check proteins using MANE genome with RefSeq IDs
-     */
-    @Test
-    public void test_01_build_mane_refseq_check_proteins() {
-        var genome = "test_GRCh38.mane.1.0.refseq.chr21";
-        var sep = build(genome);
-        // Check that all transcripts have protein IDs
-        var count = 0;
-        for(Gene g:sep.getGenome().getGenes()) {
-            for(Transcript tr: g) {
-                if( tr.isProteinCoding() ) {
-                    Assertions.assertNotNull(tr.getProteinId());
-                    count++;
-                }
-            }
-        }
-        Assertions.assertEquals(count, 213);
-    }
-
     // /**
-    //  * Rare amino acids when building with MANE genome with RefSeq IDs
+    //  * Check proteins using MANE genome with RefSeq IDs
     //  */
     // @Test
-    // public void test_01_build_mane_refseq_rare_aa() {
-        
+    // public void test_01_build_mane_refseq_check_proteins() {
+    //     var genome = "test_GRCh38.mane.1.0.refseq.chr21";
+    //     var sep = build(genome);
+    //     // Check that all transcripts have protein IDs
+    //     var count = 0;
+    //     for(Gene g:sep.getGenome().getGenes()) {
+    //         for(Transcript tr: g) {
+    //             if( tr.isProteinCoding() ) {
+    //                 Assertions.assertNotNull(tr.getProteinId());
+    //                 count++;
+    //             }
+    //         }
+    //     }
+    //     Assertions.assertEquals(count, 213);
     // }
+
+    /**
+     * Rare amino acids when building with MANE genome with RefSeq IDs
+     */
+    @Test
+    public void test_01_build_mane_refseq_rare_aa() {
+        var genome = "test_GRCh38.mane.1.0.refseq.chr21";
+        var sep = build(genome);
+    }
 
 }
